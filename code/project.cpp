7e0e858b1bb15a6441f2e68f307e56bca6c574b2
Anton Moberg <fjanteante@gmail.com>
2017-11-05 17:50:34 +0100

    moved the data extraction to constructor of tempTrender(extracts date,time,temp) and fixed fileToPath to work

--- code/project.cpp
+++ code/project.cpp
@@ -6,63 +6,15 @@
 using namespace std;
 
 int main() {
-	// string pathToFile = "smhi-opendata_Lund.csv"; //Put the path to your data file here
-	//tempTrender t(pathToFile); //Instantiate your analysis object
+	const char* pathToFile = "smhi-opendata_Lund.csv"; //Put the path to your data file here
+	tempTrender t(pathToFile); //Instantiate your analysis object
 
 	//t.tempOnDay(8, 23); //Call some functions that you've implemented
 	//t.tempOnDay(235);
 	//t.tempPerDay();
 	//t.hotCold();
 	//t.tempPerYear(2050);
-	string nC1, nC2, nC3, nC4, nC5, nC6;
-	vector<string> C1, C2, C3, C4, C5, C6;
-	string helpString;
-	ifstream file("smhi-opendata_Lund.csv");
-
-	if(file.is_open()) {
-		cout<<"File is open \n"<<endl;
-	}
-	else {
-		cout<<"Can't open the file"<<endl;
-	}
-
-	int n = 0;
-
-	for (int n = 0; n > 7; n++) {
-		getline(file, helpString);
-	}
 
 
-	// Need to find a way to push back the values of nC to the vectors C, following while loop does sort the colums right but compilers is complaining with pushback
-	while(!file.eof()) {
-			getline(file, nC1, ';');
-			// C1.push_back(nC1);
-			//cout<<nC1;
-
-			getline(file, nC2, ';');
-			C2.push_back(nC2);
-			//cout<<nC2<<endl;
-
-
-			getline(file, nC3, ';');
-			C3.push_back(nC3);
-			cout << nC3 << endl;
-
-
-			getline(file, nC4, ';');
-			// C4.push_back(nC4);
-
-			getline(file, nC5, ';');
-			// C5.push_back(nC5);
-
-			getline(file, nC6);
-			// C6.push_back(nC6);
-
-
-	}
-
-
-
-	file.close();
 	return 0;
 }
--- code/tempTrender.h
+++ code/tempTrender.h
@@ -2,12 +2,53 @@
 #define TEMPTRENDER_H
 
 #include <string>
-// #include <ifstream>
+#include <fstream>
+#include <vector>
+#include <iostream>
+
 using namespace std;
 
 class tempTrender {
-		public:
-	tempTrender(string filePath);//Construct using the specified file
+	public:
+	tempTrender(const char* filePath) {
+		vector<string> Date, Time, Temp; //vector consisting of strings to store all the values for the columns
+		string helpString; //helpstring to store junk
+		// ifstream file("smhi-opendata_Lund.csv");
+		ifstream file(filePath);
+
+		// checks if file is open and proceeds if that is the case
+		if(file.is_open()) {
+			cout<<"File "<<filePath<<" opened \n"<<endl;
+			cout<<"Initializing data extracting and sorting..."<<endl;
+			//reads the first 7 lines as junk
+			for (int n = 0; n > 7; n++) {
+				getline(file, helpString);
+			}
+
+
+			// until the end of the file read and store the values of each column in it's respective vector.
+			while(!file.eof()) {
+				string nDate, nTime, nTemp; //string to temporarily store the values of the columns
+				getline(file, nDate, ';');
+				Date.push_back(nDate);
+
+				getline(file, nTime, ';');
+				Time.push_back(nTime);
+
+				getline(file, nTemp, ';');
+				Temp.push_back(nTemp);
+
+			}
+			cout<<"Data extracted \n"<<endl;
+		}
+
+		else {
+			cout<<"Can't open the file"<<endl;
+		}
+
+		file.close();
+	}
+
 	~tempTrender() {} //Destructor
 
 	void tempOnDay(int monthToCalculate, int dayToCalculate); //Make a histogram of the temperature on this day
@@ -21,6 +62,7 @@ class tempTrender {
 	int monthToCalculate;
 	int dayToCalculate;
 	int dateToCalculate;
+	string filePath;
 
 };
 
